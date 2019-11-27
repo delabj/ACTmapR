1b904f1550e375a7917075a1a247e754df1c0a1a
Joshua de la Bruere <j.l.delabruere@gmail.com>
2019-11-27 10:12:48 -0600

    Added option to get the student name and scores only.

--- R/Project Functions.R
+++ R/Project Functions.R
@@ -1,19 +1,32 @@
 #### this reads the fixed width file into R
-ReadACT<- function(file = "fileName", year="19-20", blanks=F){
+ReadACT<- function(file = "fileName", year="19-20", blanks=F, scores_Only=T){
   df <- GetMapping(year)
   if(!is.data.frame(df)){
     stop("Given Mapping isn't a dataframe. Have you entered the year correctly?")
   }
-  #reads in the file
-  temp <- read.fwf(file=file,
-           widths = df$widthsACT,
-           col.names = df$namesACT)
- # checks to see if blank columns are needed
-  if(blanks){return(temp)}
 
-#if no removes the columns named blank#
+  #reads in the file
+  temp <- read.fwf(file=file, widths = df$widthsACT, col.names = df$namesACT)
+   # checks to see if blank columns are needed
+  if(blanks){return(temp)}
+  #if not  removes the columns named blank
   temp %>%
     select(-(starts_with("blank")))->temp
+
+#only grab studen names and scores
+  if(scores_Only){
+    temp %>%
+      select(lastName, firstName, dateOfBirth, stateID, testDate,
+             "composite"= scaleComposite,
+             "english" = scaleEnglish,
+             "mathmatics"=scaleMath,
+             "reading"= scaleReading,
+             "science"= scaleScience,
+             "writing subscore"= scoreWR
+             )->temp
+  }
+
+
   return(temp)
 }
 
@@ -27,6 +40,8 @@ GetMapping <- function(year="18-19"){
   )
   return(NULL)
 }
+
+
 
 
 ## this gets the comment for a given code
